f21a838488be976e6d01e2b6e39f627417eb20fe
Saqibur Rahman <saqibur.rahman98@gmail.com>
2023-11-25 23:47:46 +0600

    Black: Fix formatting inconsistencies

--- apps/core/models.py
+++ apps/core/models.py
@@ -9,42 +9,36 @@ user_model = settings.AUTH_USER_MODEL
 
 class BaseModel(models.Model):
     uid = models.UUIDField(
-        verbose_name=_('UUID'),
-        unique=True,
-        default=uuid.uuid4,
-        editable=False
+        verbose_name=_("UUID"), unique=True, default=uuid.uuid4, editable=False
     )
 
     created_by = models.ForeignKey(
         to=user_model,
-        verbose_name=_('Created by'),
+        verbose_name=_("Created by"),
         null=True,
         blank=True,
-        related_name='%(class)s_created',
-        on_delete=models.SET_NULL
+        related_name="%(class)s_created",
+        on_delete=models.SET_NULL,
     )
 
     created_at = models.DateTimeField(
-        verbose_name=_('Created at'),
+        verbose_name=_("Created at"),
         auto_now_add=True,
         editable=False,
-        db_index=True
+        db_index=True,
     )
 
     updated_by = models.ForeignKey(
         to=user_model,
-        verbose_name=_('Updated by'),
+        verbose_name=_("Updated by"),
         null=True,
         blank=True,
-        related_name='%(class)s_updated',
-        on_delete=models.SET_NULL
+        related_name="%(class)s_updated",
+        on_delete=models.SET_NULL,
     )
 
     updated_at = models.DateTimeField(
-        verbose_name=_('Updated at'),
-        auto_now=True,
-        null=True,
-        blank=True
+        verbose_name=_("Updated at"), auto_now=True, null=True, blank=True
     )
 
     class Meta:
